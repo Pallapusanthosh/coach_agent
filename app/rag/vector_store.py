a497b37f9d771b0a1f5189d6c5412a80bbd01c52
Pallapusanthosh <159752212+Pallapusanthosh@users.noreply.github.com>
2025-12-27 19:35:11 +0530

    Replace HuggingFaceEmbeddings with GoogleGenerativeAIEmbeddings

--- app/rag/vector_store.py
+++ app/rag/vector_store.py
@@ -1,12 +1,15 @@
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-# from app.rag.loader import load_documents
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from app.rag.loader import load_documents
+import os
 
 def build_vector_store():
     documents = load_documents()
-    embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001",
+        google_api_key=os.getenv("GOOGLE_API_KEY")
     )
+
     db = FAISS.from_documents(documents, embeddings)
     return db
